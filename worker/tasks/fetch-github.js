72c32bea70e16b48294667853ad61e893ea710e9
Todd Fichman <toddfichman@gmail.com>
2019-06-18 16:26:39 -0400

    Removing duplicates and sorting by date

--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -3,10 +3,10 @@ const fetch = require("node-fetch");
 var redis = require("redis"),
   client = redis.createClient();
 const { promisify } = require("util");
- 
+
 const setAsync = promisify(client.set).bind(client);
 
-const baseUrl = "https://jobs.github.com/positions.json";
+const baseUrl = "https://jobs.github.com/positions.json?location=los+angeles";
 
 //getting jobs from github jobs
 async function fetchGithub() {
@@ -15,7 +15,8 @@ async function fetchGithub() {
   const allJobs = [];
 
   // fetch all pages
-  while (resultsCount > 0 ) { //&& allJobs.length < 250
+  while (resultsCount > 0 && allJobs.length < 500) {
+    //&& allJobs.length < 250
     const res = await fetch(`${baseUrl}?page=${onPage}`);
     const jobs = await res.json();
     allJobs.push(...jobs);
@@ -30,8 +31,8 @@ async function fetchGithub() {
   const jrJobs = allJobs.filter(job => {
     const jobTilte = job.title.toLowerCase();
     let isJunior = true;
-    //TODO: 
-      //filter by other perameters (year of experience, description, ect)
+    //TODO:
+    //filter by other perameters (year of experience, description, ect)
 
     //algo logic
     if (
@@ -41,30 +42,31 @@ async function fetchGithub() {
       jobTilte.includes("sr.") ||
       jobTilte.includes("architect") ||
       jobTilte.includes("test") ||
-      jobTilte.includes("back end") ||
-      jobTilte.includes("backend") ||
-      jobTilte.includes("back-end") ||
+      // jobTilte.includes("back end") ||
+      // jobTilte.includes("backend") ||
+      // jobTilte.includes("back-end") ||
       jobTilte.includes(".net") ||
       jobTilte.includes("security") ||
       jobTilte.includes("cloud") ||
       jobTilte.includes("president") ||
-      jobTilte.includes("officer") 
-      
+      jobTilte.includes("officer")
     ) {
       isJunior = false;
     }
-      return isJunior;
+    return isJunior;
   });
 
   console.log("filtered jobs ", jrJobs.length);
-  // console.log("jr jobs 0", jrJobs[0]);
-  // uniq = [...new Set(array)];
-  let uniqueJobs = getUnique(jrJobs, 'id')
-  console.log("final jobs ", uniqueJobs.length);
+
+  let uniqueJobs = getUnique(jrJobs, "id");
+  console.log("unique jobs ", uniqueJobs.length);
+
+  let sortedJobs = sortByDate(uniqueJobs);
+
   //writing to redis DB with key 'github' and value of allJobs
-  //TODO: 
-    // sort final jobs array by date before writing to redis DB
-  const success = await setAsync("github", JSON.stringify(uniqueJobs));
+  //TODO:
+  // sort final jobs array by date before writing to redis DB
+  const success = await setAsync("github", JSON.stringify(sortedJobs));
 
   console.log(success);
 }
@@ -72,17 +74,26 @@ async function fetchGithub() {
 fetchGithub();
 
 function getUnique(arr, comp) {
-
   const unique = arr
-       .map(e => e[comp])
+    .map(e => e[comp])
 
-     // store the keys of the unique objects
+    // store the keys of the unique objects
     .map((e, i, final) => final.indexOf(e) === i && i)
 
     // eliminate the dead keys & store unique objects
-    .filter(e => arr[e]).map(e => arr[e]);
+    .filter(e => arr[e])
+    .map(e => arr[e]);
 
-   return unique;
+  return unique;
+}
+
+function sortByDate(arr) {
+  return arr.sort(function(a,b){
+    a = +new Date(a.created_at);
+    b = +new Date(b.created_at);
+    return a>b ? -1 : a<b ? 1 : 0;
+
+  });
 }
 
 module.exports = fetchGithub;
